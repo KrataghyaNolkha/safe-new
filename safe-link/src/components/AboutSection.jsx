950697e7bb7d259d46a61470b6be6adb08debcc4
KrataghyaNolkha <103204076+KrataghyaNolkha@users.noreply.github.com>
2025-12-31 23:47:39 +0530

    Update AboutSection.jsx

--- safe-link/src/components/AboutSection.jsx
+++ safe-link/src/components/AboutSection.jsx
@@ -35,7 +35,7 @@ const AboutSection = () => {
               <svg className="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"></path></svg>
             </div>
 
-            <h4 className="relative text-xl font-bold text-white mb-2">UCI Dataset</h4>
+            <h4 className="relative text-xl font-bold text-white mb-2">Kaggle Dataset</h4>
             <p className="relative text-sm text-gray-400 mb-8">
               Our AI breaks down the URL structure, checking for spoofed characters, odd redirects, and behavioral anomalies in real-time.
             </p>
@@ -150,5 +150,6 @@ const AboutSection = () => {
     </section>
   );
 };
+
 
 export default AboutSection;
